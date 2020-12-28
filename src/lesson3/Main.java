9fb1f25f3250ceac73e155af0ba82be3171cbacb
lagutin_vlad <lagutin_vlad@mail.ru>
2020-12-28 20:36:02 +0300

    Geek2lvl_hw3

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,7 +1,53 @@
 package lesson3;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+// Задание 1
+
+        List<String> list = new ArrayList<>(Arrays.asList("Крыло", "Фюзеляж", "Лонжерон", "Пуск", "Поехали",
+                "Летчик", "Пилот", "Самолет", "Крыло", "Крыло",
+                "Крыло", "Хвост", "Ракета", "Пилот", "Пилот"));
+        Map<String, Integer> map = new HashMap <>();
+
+        for (String i: list) {
+            map.put(i, Collections.frequency(list, i));
+        }
+
+
+        System.out.println("Уникальные слова: ");
+        map.forEach((k, v) -> {
+            if (v == 1){
+                System.out.print(k + " ");
+            }
+        });
+
+        System.out.println("\n");
+        map.forEach((k, v) -> System.out.println("Слово '" + k + "' встречается " + v + " раз."));
+
+        // Задача 2
+
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Ivanov", "+375293333333");
+        phonebook.add("Petrov", "+375294444444");
+        phonebook.add("Sidorov", "+375295555555");
+        phonebook.add("Ivanov", "+375293333311");
+        phonebook.add("Ivanov", "+375293333322");
+
+
+        phonebook.get("Ivanov");
+        phonebook.get("Sidorov");
+        phonebook.get("Petrov");
+        phonebook.get("Putin");
+
+
     }
+
+
+
+
+
+
 }
