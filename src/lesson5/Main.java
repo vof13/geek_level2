c731baba5048f0cc5bca0abd905476b4c9e3f623
lagutin_vlad <lagutin_vlad@mail.ru>
2021-01-17 23:21:03 +0300

    Geek2lvl_hw5

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,10 +1,50 @@
 package lesson5;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    static float[] arr = new float[size];
 
     public static void main(String[] args) {
-
+        oneThread();
+        manyThread();
     }
 
+    public static void oneThread ()
+    {
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        System.out.println("Один поток: " + (System.currentTimeMillis() - a));
+    }
+
+    public static void manyThread  (){
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++)
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < a2.length; i++)
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        });
+
+        t1.start();
+        t2.start();
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println("Несколько потоков: " + (System.currentTimeMillis() - a));
+
+
+    }
 }
